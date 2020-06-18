27c2b8dcfcd8913669e884856a967b4c486e8532
tom <1@qq.com>
2020-06-18 17:04:36 +0800

    lessvue

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    
+    
+    <alertCom :title='title' :content='content' :cancelE='cancelE' :confirmE='confirmE' v-if='isok'></alertCom>
+    
+  </div>
+</template>
+
+<script>
+
+import alertCom from './components/AlertCom';
+
+export default {
+  name: "App",
+  data() {
+    return {
+      title:'小心路滑',
+      content:'是否跳转到虎扑',
+      isok:true
+    }
+  },
+  components: {
+ 
+    alertCom
+  },
+  methods: {
+    cancelE:function() {
+      this.isok = false;
+    },
+    confirmE:function() {
+      location.href = 'https://www.hupu.com/';
+    }
+  },
+};
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AlertCom.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="alert">
+        <div class="zhezhao">
+            <div class="alertMain">
+                <div class="title">
+                    温馨提示:{{title}}
+                </div>
+                <div class="content">
+                    {{content}}
+                </div>
+                <div class="btnList">
+                    <div class="btn cancel" @click="cancelE">
+                        取消
+                    </div>
+                    <div class="btn confirm" @click="confirmE">确认</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+
+props:['cancelE','confirmE','title','content']
+}
+</script>
+
+<style lang="less">
+
+@zzopacity:0.3;
+@theme-color:orange;
+@default-size:18px;
+@font-color:#fff;
+
+.fullscreen() {
+    width: 100vw;
+    height: 100vw;
+    display: flex;
+}
+.defultPadding(){
+    padding: 10px
+}
+.btn() {
+    padding: 20px;
+    width: 50%;
+    text-align: center;
+    font-weight: 700;
+}
+.alert {
+   .fullscreen(); 
+    .zhezhao {
+        .fullscreen();
+        background: rgba(0, 0, 0, @zzopacity);
+        justify-content: center;
+        align-items: center;
+
+        .alertMain{
+                position: relative;
+                width: 600px;
+                height: 400px;
+                background: #fff;
+                display: flex;
+                flex-direction: column;
+                .title {
+                    font-size: @default-size;
+                    height: 50px;
+                    background: @theme-color;
+                    color: @font-color;
+                    .defultPadding();
+                    line-height: 50px;
+                    font-weight: 700;
+                }
+                .content {
+                    .defultPadding();
+                    font-size: @default-size;
+                }
+                .btnList {
+                    position: absolute;
+                    display: flex;
+                    left: 0;
+                    bottom: 0;
+                    width: 100%;
+                    .cancel {
+                        
+                        
+                        left: 0;
+
+                       .btn();
+                        background: chartreuse;
+                    }
+                    .confirm {
+                        
+                        
+                        right: 0;
+                       .btn();
+                        background:hotpink
+                    }
+                }
+            }
+
+
+    }
+   
+}
+
+</style>
